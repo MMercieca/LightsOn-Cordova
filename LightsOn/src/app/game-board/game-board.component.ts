149a45832972d0fc9b0f65a008c1de6b16805b07
Matthew Mercieca <mercieca@gmail.com>
2019-12-13 20:40:49 -0500

    Added styling to make lights look like lights

--- LightsOn/src/app/game-board/game-board.component.ts
+++ LightsOn/src/app/game-board/game-board.component.ts
@@ -7,13 +7,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./game-board.component.sass']
 })
 export class GameBoardComponent implements OnInit {
+  board: boolean[][];
+  size: number;
+  moves: number;
 
   constructor(private router: Router) { }
 
-  board: boolean[][];
-  size = 5;
-
   ngOnInit() {
+    this.size = 5;
+    this.moves = 5;
+    
     this.reset();
   }
 
@@ -23,37 +26,6 @@ export class GameBoardComponent implements OnInit {
     } else {
       return "off";
     }
-  }
-
-  toggle(row, col) {
-    if (row >= 0 && col >= 0 && row < this.size && col < this.size) {
-      this.board[row][col] = !this.board[row][col];
-    }
-  }
-
-  onPress(row, col) {
-    this.switch(row, col);
-
-    if (this.isWon) {
-      alert("Congratulations!");
-      this.router.navigateByUrl('/home');
-    }
-  }
-
-  switch(row, col) {
-    this.toggle(row, col);
-    this.toggle(row - 1, col);
-    this.toggle(row + 1, col);
-    this.toggle(row, col - 1);
-    this.toggle(row, col + 1);
-  }
-
-  randomMove() {
-    let row = Math.floor(Math.random() * this.size);
-    let col = Math.floor(Math.random() * this.size);
-    
-    console.log(row, col);
-    this.switch(row, col);
   }
 
   get isWon() {
@@ -71,6 +43,37 @@ export class GameBoardComponent implements OnInit {
     return true;
   }
 
+  toggle(row, col) {
+    if (row >= 0 && col >= 0 && row < this.size && col < this.size) {
+      this.board[row][col] = !this.board[row][col];
+    }
+  }
+
+  switch(row, col) {
+    this.toggle(row, col);
+    this.toggle(row - 1, col);
+    this.toggle(row + 1, col);
+    this.toggle(row, col - 1);
+    this.toggle(row, col + 1);
+  }
+
+  onPress(row, col) {
+    this.switch(row, col);
+
+    if (this.isWon) {
+      alert("Congratulations!");
+      this.router.navigateByUrl('/home');
+    }
+  }
+
+  randomMove() {
+    let row = Math.floor(Math.random() * this.size);
+    let col = Math.floor(Math.random() * this.size);
+    
+    console.log(row, col);
+    this.switch(row, col);
+  }
+
   reset() {
     this.board = [];
     for (let row = 0; row < this.size; row++) {
@@ -80,7 +83,7 @@ export class GameBoardComponent implements OnInit {
       }
     }
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < this.moves; i++) {
       this.randomMove();
 
       if (this.isWon) {
